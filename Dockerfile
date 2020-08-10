7a9042762522fec28db7b55cc4829487d877fc35
Heather <heather@talky.io>
2020-08-10 14:04:28 -0700

    rename vars to vercel

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM node:alpine
 
 LABEL org.label-schema.version=latest
-LABEL org.label-schema.vcs-url="https://github.com/one000mph/drone-now"
+LABEL org.label-schema.vcs-url="https://github.com/one000mph/drone-vercel"
 LABEL org.label-schema.name="drone-now"
-LABEL org.label-schema.description="Deploying to now.sh with Drone CI"
+LABEL org.label-schema.description="Deploying to Vercel with Drone CI"
 LABEL org.label-schema.vendor="Heather Young"
-LABEL org.label-schema.schema-version="1.2"
+LABEL org.label-schema.schema-version="1.3"
 
-RUN npm install -g --unsafe-perm now@latest
+RUN npm install -g vercel@latest
 
 ADD script.sh /bin/
 RUN chmod +x /bin/script.sh
--- script.sh
+++ script.sh
@@ -1,60 +1,60 @@
 #!/bin/sh
 set -e
 
-NOW_DEPLOY_OPTIONS=" --no-clipboard"
-NOW_AUTH=""
+VERCEL_DEPLOY_OPTIONS=" --no-clipboard"
+VERCEL_AUTH=""
 
 # Get the token or error
-if [ -z "$PLUGIN_NOW_TOKEN" ]; then
+if [ -z "$PLUGIN_VERCEL_TOKEN" ]; then
     # No explicit token provided, check for secret
-    if [ -z "$NOW_TOKEN" ]; then
-        echo "> Error!! either the parameter now_token or the secret NOW_TOKEN is required!"
+    if [ -z "$VERCEL_TOKEN" ]; then
+        echo "> Error!! either the parameter vercel_token or the secret VERCEL_TOKEN is required!"
         exit 1;
     else
-        PLUGIN_NOW_TOKEN="$NOW_TOKEN"
+        PLUGIN_VERCEL_TOKEN="$VERCEL_TOKEN"
     fi
 fi
 
 if [ -n "$PLUGIN_SCOPE" ]; then
     echo "> adding custom scope $PLUGIN_SCOPE"
-    NOW_SCOPE_OPTION="--scope $PLUGIN_SCOPE"
+    VERCEL_SCOPE_OPTION="--scope $PLUGIN_SCOPE"
 else
     echo "> No custom scope provided."
 fi
 
-NOW_AUTH="$NOW_AUTH --token $PLUGIN_NOW_TOKEN $NOW_SCOPE_OPTION"
+VERCEL_AUTH="$VERCEL_AUTH --token $PLUGIN_VERCEL_TOKEN $VERCEL_SCOPE_OPTION"
 
 if [ -n "$PLUGIN_DIRECTORY" ]; then
-    echo "> Deploying $PLUGIN_DIRECTORY on now.sh…"
+    echo "> Deploying $PLUGIN_DIRECTORY on Vercel…"
 fi
 
 if [ -n "$PLUGIN_LOCAL_CONFIG" ]; then
     echo "> using local config called $PLUGIN_LOCAL_CONFIG"
-    NOW_DEPLOY_OPTIONS="${NOW_DEPLOY_OPTIONS} -A ../$PLUGIN_LOCAL_CONFIG"
-    NOW_DEPLOYMENT_URL=$(now $NOW_AUTH $NOW_DEPLOY_OPTIONS $PLUGIN_DIRECTORY) &&
-    echo "> Success! Deployment complete to $NOW_DEPLOYMENT_URL";
+    VERCEL_DEPLOY_OPTIONS="${VERCEL_DEPLOY_OPTIONS} -A ../$PLUGIN_LOCAL_CONFIG"
+    VERCEL_DEPLOYMENT_URL=$(vercel $VERCEL_AUTH $VERCEL_DEPLOY_OPTIONS $PLUGIN_DIRECTORY) &&
+    echo "> Success! Deployment complete to $VERCEL_DEPLOYMENT_URL";
 
     if [ -n "$PLUGIN_PROD" ]; then
         echo "> Production deploy…" &&
-        PROD_SUCCESS_MESSAGE=$(now deploy --prod $NOW_AUTH $NOW_DEPLOY_OPTIONS $PLUGIN_DIRECTORY) &&
+        PROD_SUCCESS_MESSAGE=$(vercel deploy --prod $VERCEL_AUTH $VERCEL_DEPLOY_OPTIONS $PLUGIN_DIRECTORY) &&
         echo "$PROD_SUCCESS_MESSAGE"
     fi
 else
-    echo "> No local config provided, now will not use a local config"
+    echo "> No local config provided, vercel will not use a local config"
 
     if [ -n "$PLUGIN_DEPLOY_NAME" ]; then
         echo "> adding deploy_name $PLUGIN_DEPLOY_NAME"
-        NOW_DEPLOY_OPTIONS="${NOW_DEPLOY_OPTIONS} --name $PLUGIN_DEPLOY_NAME"
+        VERCEL_DEPLOY_OPTIONS="${VERCEL_DEPLOY_OPTIONS} --name $PLUGIN_DEPLOY_NAME"
     else
         echo "> No deployment name provided. The directory will be used as the name"
     fi
 
-    NOW_DEPLOYMENT_URL=$(now $NOW_AUTH $NOW_DEPLOY_OPTIONS $PLUGIN_DIRECTORY) &&
-    echo "> Success! Deployment complete to $NOW_DEPLOYMENT_URL";
+    VERCEL_DEPLOYMENT_URL=$(vercel $VERCEL_AUTH $VERCEL_DEPLOY_OPTIONS $PLUGIN_DIRECTORY) &&
+    echo "> Success! Deployment complete to $VERCEL_DEPLOYMENT_URL";
 
     if [ -n "$PLUGIN_PROD" ]; then
         echo "> Production deploy…" &&
-        PROD_SUCCESS_MESSAGE=$(now deploy --prod $NOW_AUTH $NOW_DEPLOY_OPTIONS $PLUGIN_DIRECTORY) &&
+        PROD_SUCCESS_MESSAGE=$(vercel deploy --prod $VERCEL_AUTH $VERCEL_DEPLOY_OPTIONS $PLUGIN_DIRECTORY) &&
         echo "$PROD_SUCCESS_MESSAGE"
         ## Check exit code
         rc=$?;
@@ -69,10 +69,10 @@ fi
 if [ "$PLUGIN_CLEANUP" == "true" ]; then
     if [ -n "$PLUGIN_PROD" ]; then
         echo "> Cleaning up old deployments…" &&
-        PROD_SUCCESS_MESSAGE=$(now rm --safe --yes $NOW_AUTH $PLUGIN_PROD) &&
+        PROD_SUCCESS_MESSAGE=$(vercel rm --safe --yes $VERCEL_AUTH $PLUGIN_PROD) &&
         echo "$PROD_SUCCESS_MESSAGE"
     else
-        echo "> Warning!! You must set the prod parameter when using the cleanup parameter so that now.sh knows which deployments to remove!"
+        echo "> Warning!! You must set the prod parameter when using the cleanup parameter so that vercel knows which deployments to remove!"
     fi
 fi
 
@@ -82,5 +82,5 @@ if [[ $rc != 0 ]]; then
     echo "> non-zero exit code $rc" &&
     exit $rc
 else
-    echo $'\n'"> Successfully deployed! $NOW_DEPLOYMENT_URL"$'\n'
+    echo $'\n'"> Successfully deployed! $VERCEL_DEPLOYMENT_URL"$'\n'
 fi
